56da4a08dec1e85b67c3dc0d67896906da96740b
Mark Vinnichuk <markvinni71@gmail.com>
2025-06-25 00:29:01 +0300

    + 2 new blocks + 2 new items + my iq (maybe)

--- src/main/java/com/mvks/TestMod.java
+++ src/main/java/com/mvks/TestMod.java
@@ -1,16 +1,22 @@
 package com.mvks;
 
+import com.mvks.block.ModBlocks;
+import com.mvks.item.ModItemGroups;
+import com.mvks.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestMod implements ModInitializer {
-	public static final String MOD_ID = "testmod01";
+	public static final String MOD_ID = "testmod";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 	}
 }

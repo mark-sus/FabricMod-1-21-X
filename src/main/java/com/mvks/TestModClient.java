e16e21a162e22342beeab5b2f84554675f418639
Mark Vinnichuk <markvinni71@gmail.com>
2025-06-24 22:47:07 +0300

    Added all

---
+++ src/main/java/com/mvks/TestModClient.java
@@ -0,0 +1,10 @@
+package com.mvks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

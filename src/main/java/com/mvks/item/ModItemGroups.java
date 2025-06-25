3241158cd4eb30843aa1a0ea7fc13b9777c9207a
Mark Vinnichuk <markvinni71@gmail.com>
2025-06-25 12:34:58 +0300

    new + 2 new ores +2 crafts + loot tables - my life

---
+++ src/main/java/com/mvks/item/ModItemGroups.java
@@ -0,0 +1,36 @@
+package com.mvks.item;
+
+import com.mvks.TestMod;
+import com.mvks.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+
+    public static final ItemGroup PINK_GARNET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TestMod.MOD_ID, "pink_garnet_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_GARNET))
+                    .displayName(Text.translatable("itemgroup.testmod.pink_garnet_items"))
+                    .entries((displayContext, entries) -> {
+                      entries.add(ModItems.PINK_GARNET);
+                      entries.add(ModItems.RAW_PINK_GARNET);
+
+                      entries.add(ModBlocks.PINK_GARNET_BLOCK);
+                      entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+
+                      entries.add(ModBlocks.PINK_GARNET_ORE);
+                      entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+
+                    }).build());
+
+
+    public static void  registerItemGroups(){
+        TestMod.LOGGER.info("Register item groups for" + TestMod.MOD_ID);
+    }
+
+}
